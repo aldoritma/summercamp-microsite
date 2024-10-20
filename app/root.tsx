d88a349478f623cae37803f596f4b71eddf64843
aldo <aldo.ariandhika@credibook.com>
2024-10-20 21:27:58 +0700

    chore: change tailwind import url

--- app/root.tsx
+++ app/root.tsx
@@ -14,7 +14,7 @@ import {
 import favicon from '~/assets/favicon.svg';
 import resetStyles from '~/styles/reset.css?url';
 import appStyles from '~/styles/app.css?url';
-import tailwindCss from './styles/tailwind.css?url';
+import tailwindCss from '~/styles/tailwind.css?url';
 import {PageLayout} from '~/components/PageLayout';
 import {FOOTER_QUERY, HEADER_QUERY} from '~/lib/fragments';
 
